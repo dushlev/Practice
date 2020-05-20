1a26c401e025c78f6ea346f4a98532eb62b44ec4
dushlev <64083000+dushlev@users.noreply.github.com>
2020-05-20 22:42:49 +0300

    Task 2 Database

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Data.Entity;
 
 namespace task1
 {
@@ -19,7 +21,32 @@ namespace task1
                 Console.WriteLine("An exception has occurred");
                 Console.WriteLine(ex.Message);
             }
-            Console.WriteLine(str);
+            //Console.WriteLine(str);
+            try
+            {
+                using (UserContext db = new UserContext())
+                {
+                    Expression exp1 = new Expression { ExpressionStr = str };
+                    db.Expressions.Add(exp1);
+                    
+                    db.SaveChanges();
+                    Console.WriteLine("Objects saved successfully");
+
+                    var users = db.Expressions;
+                    
+                    var lastResult = db.Expressions.OrderBy(b => b.Id)
+.Skip(Math.Max(0, db.Expressions.OrderBy(b => b.Id).Count() - 5));
+                    foreach (Expression exp in lastResult)
+                    {
+                        Console.WriteLine("{0}.{1}", exp.Id, exp.ExpressionStr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception has occurred");
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadKey();
         }
     }
