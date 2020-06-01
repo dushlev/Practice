62d9a3dca2277e728a6b7beb2da006d0df355759
dushlev <64083000+dushlev@users.noreply.github.com>
2020-06-01 10:35:18 +0300

    Task 4 Windows Forms application

---
+++ WindowsFormsApp/Form.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(75, 23);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(547, 194);
+            this.textBoxOutput.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(75, 283);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(547, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(457, 345);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(79, 30);
+            this.buttonRead.TabIndex = 2;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.textBoxOutput);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonRead;
+    }
+}
+
---
+++ WindowsFormsApp/Form.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using SharedProject;
+
+namespace WindowsFormsApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            MessageBox.Show(
+        "Enter the line with expression in the textBox and click the Read button.",
+        "ReadMe");
+
+        }
+        string str = null, strFormatobj = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                strFormatobj = textBoxInput.Text;
+                FormatExpression obj = new FormatExpression();
+                obj.str = strFormatobj;
+                str = obj.FormatStr();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+        ex.Message,
+        "An exception has occurred");
+            }
+
+            try
+            {
+                string strResult = null;
+                using (UserContext db = new UserContext())
+                {
+                    Expression exp1 = new Expression { ExpressionStr = str };
+                    db.Expressions.Add(exp1);
+
+                    db.SaveChanges();
+                    var users = db.Expressions;
+
+
+                    var lastResult = db.Expressions.OrderBy(b => b.Id)
+.Skip(Math.Max(0, db.Expressions.OrderBy(b => b.Id).Count() - 5));
+                    foreach (Expression exp in lastResult)
+                    {
+                        strResult += exp.Id.ToString() + ". " + exp.ExpressionStr + "\r\n";
+                    }
+                }
+                textBoxOutput.Text = strResult;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+           ex.Message,
+           "An exception has occurred");
+            }
+        }
+    }
+}
